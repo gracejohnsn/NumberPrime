112f8074145f25ec85e0365d56f38c0b9afa9af4
Harsheen <khandpur@wisc.edu>
2018-04-12 10:59:24 -0500

    conversions

---
+++ public/conversionsView/conversions.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('conversions', ['ngRoute']);
+angular.
+  module('conversions').
+  component('conversions', {
+    templateUrl: 'conversionsView/conversions.html',
+    controller: ['conversionsController',
+      function ConversionsController() {
+
+	}
+    ]
+  });
