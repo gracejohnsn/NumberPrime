909e8e79bc008d0b5dbb6146e7818d9069841110
Mohana Kona <Kona@Mohanas-MacBook-Air.local>
2018-04-12 18:30:39 -0500

    added report view at the end of problem set

--- public/volumeView/volume.js
+++ public/volumeView/volume.js
@@ -1,13 +1,23 @@
 'use strict';
 
-angular.module('volume', ['ngRoute']);
+var app = angular.module('volume', ['ngRoute']);
 angular.
   module('volume').
   component('volume', {
     templateUrl: 'volumeView/volume.html',
-    controller: ['VolumeController',
-      function VolumeController() {
-
-	}
-    ]
   });
+
+app.controller('volume', function($scope) {
+/*
+    $scope.prob = 1;
+    $scope.complete = "false";
+
+    if($scope.prob == 6) {
+      $scope.complete = "true";
+    }
+    $scope.next = function() {
+      $scope.prob = $scope.prob + 1;
+    }
+    */
+
+});
