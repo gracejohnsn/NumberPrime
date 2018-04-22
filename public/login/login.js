efac33c83fd077eb88b6399ff70d825654d7d51c
Michael Scott <michael.yates.scott@gmail.com>
2018-04-22 11:26:32 -0500

    Login is working with redirect now, should take you to login page if you're not signed in, have to click button twice for me in Chrome though...

--- public/login/login.js
+++ public/login/login.js
@@ -7,7 +7,29 @@ angular.
     templateUrl: 'login/login.html',
     controller: ['LoginController',
       function LoginController() {
-      
-	}
+        
+      }
     ]
   });
+
+  $(document).ready(
+    function() {
+      firebase.auth().getRedirectResult().then(
+        function(result) {
+          if(result.user) {
+            window.location = "/#!/Dashboard";
+          }
+        }
+      )
+    }
+  );
+
+function login() {
+  var provider = new firebase.auth.GoogleAuthProvider();
+  provider.setCustomParameters({
+    prompt: 'select_account'
+  });
+
+  firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION).then(
+  firebase.auth().signInWithRedirect(provider));
+}
