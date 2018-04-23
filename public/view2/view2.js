aed3d8411c1ec8ca64c0efbabb32fcf280ed590c
Michael Scott <michael.yates.scott@gmail.com>
2018-04-23 16:38:24 -0500

    Added signing up as student functionality

--- public/view2/view2.js
+++ public/view2/view2.js
@@ -11,3 +11,38 @@ angular.
 	}
     ]
   });
+
+  function createAccount() {
+    if(document.getElementById('teacherCheck').checked) {
+      // create teacher
+      var user = firebase.auth().currentUser;
+      var name = user.displayName.trim();
+      var firstName = name.substr(0, name.indexOf(" "));
+      var surName = name.substr(name.lastIndexOf(" ") + 1, );
+      var email = document.getElementById('emailEntry').value;
+      email = email == "" ? user.email : email;
+      User.createUser(firebase, user.uid, firstName, surName, email, new Date(), 
+        "teacher", {"classList": [], "teacherDesc": ""}).then(
+          function() {
+            window.location = "/#!/DashboardTeach";
+          }, function(err) {
+            alert("There was an error, please try again.");
+          }
+        );
+    } else {
+      var user = firebase.auth().currentUser;
+      var name = user.displayName.trim();
+      var firstName = name.substr(0, name.indexOf(" "));
+      var surName = name.substr(name.lastIndexOf(" ") + 1, );
+      var email = user.email;
+      var gradeLevel = document.querySelector('input[name = "grade"]:checked').value;
+      User.createUser(firebase, user.uid, firstName, surName, email, new Date(),
+        "student", {"classId": -1, "gradeLevel": gradeLevel}).then(
+          function() {
+            window.location = "/#!/Dashboard";
+          }, function(err) {
+            alert("There was an error, please try again.")
+          }
+        );
+    }
+  }
